25ab8dd17bcd43e4a7fe80a31a117ab656076092
Ibrahim Sulemana <ibeevpersie@gmail.com>
2022-06-12 13:48:34 +0000

    Simple JS Calculator

---
+++ Advance Calculator/calculator.js
@@ -0,0 +1,8 @@
+const screen = document.getElementById('screen'),
+    number = document.getElementsByClassName('number'),
+    operator = document.getElementsByClassName('operator'),
+    result = document.getElementById('result')
+    clr = document.getElementById('clr'),
+    resultDisplay = false;
+
+screen.style.display = number;
